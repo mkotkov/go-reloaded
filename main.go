5295bc37e1d2eb7737fed59f1e30a751b51db00b
mkotkov <maximkotkov@gmail.com>
2023-09-18 15:40:22 +0300

    finale

--- main.go
+++ main.go
@@ -11,119 +11,98 @@ import (
 func main() {
 	changeFile()
 }
-
-
-func changeFile(){
-
+//read sample.txt args[0], change and save to result.txt 
+func changeFile() {
 	args := os.Args[1:]
 	sampleFilePath := args[0]
 	resultFilePath := args[1]
 
 	file, err := os.ReadFile(sampleFilePath)
-	
-	fileString := string(file)
-	
-	fileString = cap(fileString)
-	fileString = up(fileString)
 
-    // cleanedText := clean(fileString)
-	
+	fileString := string(file)
+	fileString = processText(fileString)
+	fileString = clean(fileString)
+
 	data := []byte(fileString)
 
-	err = os.WriteFile(resultFilePath, []byte(data), 0644) // Write the modified content to the result file
+	err = os.WriteFile(resultFilePath, []byte(data), 0o644) // Write the modified content to the result file
 	if err != nil {
 		fmt.Println("Error writing to the result file:", err)
 		return
 	}
 
 	fmt.Println("File successfully written to", resultFilePath)
-
 }
 
+// chacking and change text
+func processText(input string) string {
+	inputArr := strings.Fields(string(input)) // splits the string
+	var count int
 
-
-func cap(input string) string{
-	
-	re := regexp.MustCompile(`\b\w+\s+\(cap\)`)
-
-	output := re.ReplaceAllStringFunc(input, func(matched string) string {
-		// Получите слово перед "(cap)"
-		words := strings.Fields(matched) // Разделите строку на слова
-		if len(words) > 1 {
-			word := words[len(words)-2]  // Получите предпоследнее слово
-			capitalized := strings.ToUpper(word[:1]) + word[1:] // Сделайте первую букву заглавной
-			return strings.Replace(matched, word, capitalized, 1)
+	for i := 0; i < len(inputArr); i++ { // takes every element separately from the slice input
+		if inputArr[i] == "(hex)" {
+			r, _ := strconv.ParseInt(inputArr[i-1], 16, 64) // converts the string from hex to dec
+			inputArr[i-1] = strconv.FormatInt(r, 10)        // converts int to string
 		}
-		return matched
-	})
+		if inputArr[i] == "(bin)" {
+			q, _ := strconv.ParseInt(inputArr[i-1], 2, 64) // converts the string from bin to dec
+			inputArr[i-1] = strconv.FormatInt(q, 10)       // converts int to string
+		}
 
-	return output
-}
+		if inputArr[i] == "(cap)" {
+			inputArr[i-1] = strings.Title(inputArr[i-1])
+		}
 
+		if inputArr[i] == "(cap," {
+			count, _ = strconv.Atoi(strings.Replace(inputArr[i+1], ")", "", 1))
+			for e := 1; e <= count; e++ {
+				inputArr[i-e] = strings.Title(inputArr[i-e])
+			}
+		}
 
-func up(input string) string{
-	n := 1
-	// re := regexp.MustCompile(`(\w+\s+\(cap(?:,\s*(\d+))?\))`)
-	exp := fmt.Sprintf(`((\w+\s+){%d}\(cap(?:,\s*(\d+))?\))`, n)
+		if inputArr[i] == "(low)" {
+			inputArr[i-1] = strings.ToLower(inputArr[i-1])
+		}
 
-	re:= regexp.MustCompile(exp)
+		if inputArr[i] == "(low," {
+			count, _ = strconv.Atoi(strings.Replace(inputArr[i+1], ")", "", 1))
+			for e := 1; e <= count; e++ {
+				inputArr[i-e] = strings.ToLower(inputArr[i-e])
+			}
+			inputArr[i] = strings.Replace(inputArr[i], inputArr[i], "", 1)
+			inputArr[i+1] = strings.Replace(inputArr[i+1], inputArr[i+1], "", 1)
+		}
+		if inputArr[i] == "(up)" {
+			inputArr[i-1] = strings.ToUpper(inputArr[i-1])
+		}
+		if inputArr[i] == "(up," {
+			count, _ = strconv.Atoi(strings.Replace(inputArr[i+1], ")", "", 1))
+			for e := 1; e <= count; e++ {
+				inputArr[i-e] = strings.ToUpper(inputArr[i-e])
+			}
+			inputArr[i] = strings.Replace(inputArr[i], inputArr[i], "", 1)
+			inputArr[i+1] = strings.Replace(inputArr[i+1], inputArr[i+1], "", 1)
+		}
+	}
 
-	output := re.ReplaceAllStringFunc(input, func(matched string) string {
+	justString := strings.Join(inputArr, " ")
+
+	// check and change article
+	re := regexp.MustCompile(`a\s+([aeiouhAEIOUH])`)
+
+	output := re.ReplaceAllStringFunc(justString, func(matched string) string {
 		submatches := re.FindStringSubmatch(matched)
-
-		if len(submatches) >= 2 {
-			wordList := strings.Split(submatches[1], " ")
-			numWords := 1 
-
-			if len(submatches) == 3 {
-				// Если указано число, преобразуем его в int
-				n, err := strconv.Atoi(submatches[2])
-				if err == nil {
-					numWords = n
-				}
-			}
-
-			if numWords > len(wordList) {
-				numWords = len(wordList)
-			}
-
-			for i := 0; i < numWords; i++ {
-				word := wordList[i]
-				capitalized := strings.ToUpper(word[:1]) + word[1:]
-				wordList[i] = capitalized
-			}
-
-			return strings.Join(wordList, " ")
+		if len(submatches) > 1 {
+			return "an " + strings.TrimSpace(submatches[1])
 		}
-
 		return matched
 	})
-
 	return output
 }
 
-// func low(input string) string{
-// 	re := regexp.MustCompile(`\b\w+\s+\(up\)`)
-	
-// 	output := re.ReplaceAllStringFunc(input, func(matched string) string {
-		
-// 		words := strings.Fields(matched)
-		
-// 		if len(words) > 1 {
-// 			word := words[len(words)-2]                         
-// 			capitalized := strings.ToUpper(word)
-// 			return strings.Replace(matched, word, capitalized, 1)
-// 		}
-// 		return matched
-// 	})
-
-// 	return output
-// }
-
-
-// func clean(input string) string{
-// 	re := regexp.MustCompile(`\s+([,.;:!?])| \(([^)]+)\)`)
-//   	output := re.ReplaceAllString(input, "$1")
-// 	return output
-// }
-
+// clean extra spacing, double-spacing and hashtags (cap, low, etc.)
+func clean(input string) string {
+	re := regexp.MustCompile(`\s+([,.;:!?])|\s+\(([^)]+)\)|('...''!?')|\w\s{2}`)
+	output := re.ReplaceAllString(input, "$1")
+	return output
+}
